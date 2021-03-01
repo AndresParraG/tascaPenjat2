8f615e4ee45e9a88bb39ed3ed7502910218e2bc7
Andres <aparrag@cifpfbmoll.eu>
2021-03-01 17:53:14 +0100

    preparacion para tests

---
+++ test/TestPuntuacio.java
@@ -0,0 +1,12 @@
+package test;
+
+import com.jaume.penjat.Puntuacio;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestPuntuacio {
+
+    private Puntuacio puntuacio;
+}
