336f008f451fb325ccd11976ab1f4f1fa013b43c
Andres <aparrag@cifpfbmoll.eu>
2021-03-02 19:45:15 +0100

    gitflow-feature-stash: test-paraulaDificultat1
    añadido beforeEach que inicializa una instancia de puntuacio antes de realiar cualquier test

--- test/TestPuntuacio.java
+++ test/TestPuntuacio.java
@@ -10,30 +10,31 @@ class TestPuntuacio {
 
     private Puntuacio puntuacio;
 
+    @BeforeEach
+    void inicialitzarPuntuacio() {
+        this.puntuacio = new Puntuacio();
+    }
+
     @Test
     void iniciarPartidaDificultat1() {
-        puntuacio = new Puntuacio();
         puntuacio.getParaulaSecretaDificultat(1);
         assertEquals(5, puntuacio.getIntents());
     }
 
     @Test
     void iniciarPartidaDificultat2() {
-        puntuacio = new Puntuacio();
         puntuacio.getParaulaSecretaDificultat(2);
         assertEquals(4, puntuacio.getIntents());
     }
 
     @Test
     void iniciarPartidaDificultat3() {
-        puntuacio = new Puntuacio();
         puntuacio.getParaulaSecretaDificultat(3);
         assertEquals(3, puntuacio.getIntents());
     }
 
     @Test
     void iniciarPartidaDificultatErrada() {
-        puntuacio = new Puntuacio();
         assertEquals("err", puntuacio.getParaulaSecretaDificultat(0));
     }
 }
